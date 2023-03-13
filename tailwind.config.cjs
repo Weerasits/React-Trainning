ebcb4d98035e7f19e0e68b16f742f5f2b22ec470
Weerasits <96558513+Weerasits@users.noreply.github.com>
2023-03-13 23:22:32 +0700

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily : {
+      bai: ["Bai Jamjuree", "sans-serif"],
+    },
+  },
+  plugins: [],
+}
